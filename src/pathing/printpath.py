3c5cdf2e594941ded8e720bf68826f5e30c0e8b9
murphyt <sttam09@moravian.edu>
2019-03-20 15:14:25 -0400

    Made changes to have linter be happy

--- src/pathing/printpath.py
+++ src/pathing/printpath.py
@@ -1,13 +1,17 @@
+"""This module prints a path"""
 import csv
 import os.path
 
-
+#pylint: disable=R0903
 class Pathing:
-	def get_test_file(self):
-		my_path = os.path.abspath(os.path.dirname(__file__))
-		new_path = os.path.join(my_path, "../../tests/test_files/test.csv")
-		with open(new_path) as f:
-			test = csv.reader(f)
-			for line in test:
-				for word in line:
-					return(word)
+    """Example of pathing"""
+    # pylint: disable=R0201
+    def get_test_file(self):
+        """method for showing pathing"""
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        new_path = os.path.join(my_path, "../../tests/test_files/test.csv")
+        with open(new_path) as file:
+            test = csv.reader(file)
+            for line in test:
+                for word in line:
+                    return word
