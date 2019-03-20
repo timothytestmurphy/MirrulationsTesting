ff43b4b6cfba3052ed5bedd5d216faa03ba8422a
murphyt <sttam09@moravian.edu>
2019-03-20 14:38:19 -0400

    added linting, coverage, and data in requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pathing',
+    python_requires='>=3.7.2',
+    install_requires=['pytest-cov==2.6.1',
+                      'pylint==2.3.1',
+                      'pytest==4.3.0'],
     packages=find_packages('src'),
     package_dir={'': 'src'}
 )
